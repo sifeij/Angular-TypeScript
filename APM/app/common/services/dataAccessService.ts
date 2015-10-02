8717e133211c293450a6fda6ff11f36203826b99
sifeij <sifeij@gmail.com>
2015-10-01 18:46:31 -0700

    Create dataAccessService class with one method

--- APM/app/common/services/dataAccessService.ts
+++ APM/app/common/services/dataAccessService.ts
@@ -1,9 +1,23 @@
 module app.common {
 	interface IDataAccessService {
+		getProductResource() : ng.resource.IResourceClass<IProductResource>;
+	}
+	
+	interface IProductResource 
+		extends ng.resource.IResource<app.domain.IProduct> {
 		
 	}
 	
-	interface IProductResource extends ng.resource.IResource<app.domain.IProduct> {
-		
-	}
+	export class DataAccessService
+		implements IDataAccessService {
+			
+			static $inject = ["$resource"];
+			constructor(private $resource: ng.resource.IResourceService) {
+				
+			}
+			
+			getProductResource() : ng.resource.IResourceClass<IProductResource>{
+				return this.$resource("/api/products/:productId");
+			}
+		}
 }
