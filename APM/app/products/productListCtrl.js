8d6415bc2e95f71d0207ea4b57b1cc83f41e63b0
sifeij <sifeij@gmail.com>
2015-10-01 17:13:04 -0700

    Use entity class in angular controller to add one more product with discount price

--- APM/app/products/productListCtrl.js
+++ APM/app/products/productListCtrl.js
@@ -35,6 +35,9 @@ var app;
                         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
                     }
                 ];
+                var newProduct = new app.domain.Product(3, "Saw", "TBX-002", new Date(2002, 3, 1), 16.95, "15-inch steel blade hand saw", "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png");
+                newProduct.price = newProduct.calculateDiscount(10);
+                this.products.push(newProduct);
             }
             ProductListCtrl.prototype.toggleImage = function () {
                 this.showImage = !this.showImage;
--- APM/app/products/productListCtrl.ts
+++ APM/app/products/productListCtrl.ts
@@ -2,14 +2,14 @@ module app.productList {
 	interface IProductListModel {
 		title: string;
 		showImage: boolean;
-		products: any[];
+		products: app.domain.IProduct[];
 		toggleImage(): void;
 	}
 
 	class ProductListCtrl implements IProductListModel {
 		title: string;
 		showImage: boolean;
-		products: any[];
+		products: app.domain.IProduct[];
 
 		constructor() {
 			this.title = "Product List";
@@ -42,7 +42,14 @@ module app.productList {
 					"price": 8.99,
 					"imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
 				}
-			]
+			];
+			var newProduct = new app.domain.Product
+								(3, "Saw", "TBX-002",
+								new Date(2002, 3, 1), 16.95,
+								"15-inch steel blade hand saw",
+								"http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png");
+			newProduct.price = newProduct.calculateDiscount(10);
+			this.products.push(newProduct);
 		}
 
 		toggleImage(): void {
